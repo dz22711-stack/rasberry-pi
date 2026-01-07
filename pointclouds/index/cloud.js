3567a46f0ef313ffa3dfec899e340514c4d3edf1
dz22711-stack <dz22711@shibaura-it.ac.jp>
2026-01-07 19:02:54 +0900

    Update data

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 800000,
+    "boundingBox": {
+        "lx": -1.993114948272705,
+        "ly": -6.007460117340088,
+        "lz": -1.464,
+        "ux": 19.478885051727298,
+        "uy": 15.464539882659914,
+        "uz": 20.008000000000004
+    },
+    "tightBoundingBox": {
+        "lx": -1.993114948272705,
+        "ly": -6.007460117340088,
+        "lz": -1.464,
+        "ux": 10.855885051727296,
+        "uy": 15.464539882659914,
+        "uz": 2.581
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.18595297634601594,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
